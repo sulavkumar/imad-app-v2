aae4c5227c8bfbb1b6e89b399b0b639970135e2e
sulavkumar <sulav.kumar1@gmail.com>
2017-03-22 04:49:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+   res.send('article one was requested and will be served here'); 
+});
+
+app.get('/article-two', function (req, res) {
+   res.send('article two was requested and will be served here'); 
+});
+
+app.get('/article-three', function (req, res) {
+   res.send('article three was requested and will be served here'); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
